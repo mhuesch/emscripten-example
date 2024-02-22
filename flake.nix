be5f13c444795025b32ab8b5564814f407bb70f5
Michael Hueschen <m@mhueschen.dev>
2024-02-22 07:39:50 -0700

    flakify

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      supported-systems = [ "x86_64-linux" "aarch64-linux" ];
+    in
+    flake-utils.lib.eachSystem supported-systems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell =
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              emscripten
+              gdb
+              nodejs
+            ];
+          };
+      });
+
+}
